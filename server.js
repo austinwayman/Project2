1376625f9cf6f19a524bc0048d0d672b57bb6807
Austin Wayman <awguitar@msn.com>
2018-11-26 09:49:03 -0700

    remove duplicate server listen

--- server.js
+++ server.js
@@ -32,19 +32,19 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+//   app.listen(PORT, function() {
+//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+//   });
+  server.listen(process.env.PORT || 8080)
 });
 
 var server = require("http").createServer(app);
 
- var io = require("socket.io").listen(server);
+var io = require("socket.io").listen(server);
 
  users = [];
  connections = [];
 
- server.listen(process.env.PORT || 8080)
 
 
  io.sockets.on('connection', function(socket){
